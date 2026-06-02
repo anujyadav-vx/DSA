6e68d2350460cebb2aad0c6fa2beca5e7309d9d9
Anuj Yadav <anujyadav@example.com>
2026-06-03 00:29:27 +0530

    py

---
+++ python/machineCoding/result.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+
+hours = [[1], [2], [3], [4],[5],[6], [7],[8]]
+result = [0,0,0,0,1,1,1,1]
+
+model = LinearRegression()
+
+model.fit(hours, result)
+
+prediction = model.predict([[5]])
+
+print("Prediction:", prediction)
---
+++ python/machineCoding/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+print("All libraries installed successfully!")
