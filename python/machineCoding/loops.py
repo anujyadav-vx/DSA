84a8924f0656998cb2ce406923a4c58700e65212
Anuj Yadav <anujyadav@example.com>
2026-06-08 20:11:14 +0530

    loop

---
+++ python/machineCoding/loops.py
@@ -0,0 +1,19 @@
+#For Loop
+for i in range(8):
+    print(i)
+
+
+for i in range(1, 8):
+    print(i)
+
+
+for i in range(1, 5, 3):
+    print(i)
+
+name = "anuj"
+for ch in name:
+    print(ch)
+
+fruits = ["apple", "mango", "banana"]
+for fruit in fruits:
+    print(fruit)    
